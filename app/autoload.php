65a1b20130755a63eacc8f0bd2578c3d6a04c3a2
Zubair Ahmed <shojeeb.nz@gmail.com>
2012-06-19 22:21:10 +1200

    symfony 2 base app

---
+++ app/autoload.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+
+    $message = <<< EOF
+<p>You must set up the project dependencies by running the following commands:</p>
+<pre>
+    curl -s http://getcomposer.org/installer | php
+    php composer.phar install
+</pre>
+
+EOF;
+
+    if (PHP_SAPI === 'cli') {
+        $message = strip_tags($message);
+    }
+
+    die($message);
+}
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+// intl
+if (!function_exists('intl_get_error_code')) {
+    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+
+    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
+}
+
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
